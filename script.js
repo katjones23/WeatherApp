3bf8f84f13284c231bfcfc1b985f1da27b588525
Katherine Jones <kejone1414@gmail.com>
2020-04-10 00:23:46 -0400

    feat: sidebar local stoage set up

--- script.js
+++ script.js
@@ -3,10 +3,6 @@
 
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
@@ -16,18 +12,62 @@
 
 $(document).ready(function () {
 
-    $("button").on("click", function () {
+    var prevSearches = [];
+    var searchHistory = $(".searchHistory");
+
+    init();
+
+    function init() {
+        var storedSearches = JSON.parse(localStorage.getItem("prevSearches"));
+
+        if (storedSearches !== null) {
+            prevSearches = storedSearches;
+        }
+
+        renderSearchHistory();
+    }
+
+    function renderSearchHistory() {
+        $(searchHistory).empty();
+
+        for (var i = 0; i < prevSearches.length; i++) {
+            var searchedCity = prevSearches[i];
+
+            var div = $("<div>");
+            
+            $(searchHistory).css("display", "block")
+            $(div).text(searchedCity);
+            $(div).addClass("card");
+            $(searchHistory).prepend(div);
+        }
+    };
+
+    $("#clear").click(function clearStorage() {
+        localStorage.clear();
+        $(searchHistory).empty();
+    })
+
+    $("#searchBtn").on("click", function () {
 
         var inputCity = $("#searchCity").val();
+
+        function saveSearch() {
+            prevSearches.push(inputCity);
+            localStorage.setItem("prevSearches", JSON.stringify(prevSearches));
+        };
+
+        saveSearch();
+
         var APIKey = "&appid=fc3e557a89901d475f37aaf29507552a";
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&units=imperial" + APIKey;
         var currentDate = moment().format('dddd MMM Do YYYY')
 
+        // Current weather
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-            .then(function(response) {
+            .then(function (response) {
                 $(".weatherCard").css("display", "block");
 
                 var iconURl = "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png";
@@ -37,7 +77,7 @@ $(document).ready(function () {
                 var temperature = response.main.temp;
                 var humidity = response.main.humidity;
                 var windSpeed = response.wind.speed;
-                
+
                 $(".cityWeather").append("<p>Temperature: " + temperature + " °F</p>");
                 $(".cityWeather").append("<p>Humidity: " + humidity + "%</p>");
                 $(".cityWeather").append("<p>Wind Speed: " + windSpeed + " MPH</p>");
@@ -48,36 +88,50 @@ $(document).ready(function () {
                     url: UVqueryURL,
                     method: "GET"
                 })
-                    .then(function(response) {
+                    .then(function (response) {
                         var UVIndex = response.value;
                         var UVIndexHTML = $("<p>").html("UV Index: ")
                         var UVBadge = $("<span>").addClass("badge").text(UVIndex)
 
                         if (UVIndex < 3) {
-                            $(UVBadge).css({"background-color": "green", "color": "white"})
+                            $(UVBadge).css({ "background-color": "green", "color": "white" })
                         } else if (UVIndex < 6 && UVIndex >= 3) {
-                            $(UVBadge).css({"background-color": "yellow", "color": "white"})
+                            $(UVBadge).css({ "background-color": "yellow", "color": "white" })
                         } else if (UVIndex < 8 && UVIndex >= 6) {
-                            $(UVBadge).css({"background-color": "orange", "color": "white"})
+                            $(UVBadge).css({ "background-color": "orange", "color": "white" })
                         } else if (UVIndex < 11 && UVIndex >= 8) {
-                            $(UVBadge).css({"background-color": "red", "color": "white"})
+                            $(UVBadge).css({ "background-color": "red", "color": "white" })
                         } else if (UVIndex >= 11) {
-                            $(UVBadge).css({"background-color": "violet", "color": "white"})
+                            $(UVBadge).css({ "background-color": "violet", "color": "white" })
                         }
 
                         $(".cityWeather").append(UVIndexHTML);
                         $(UVIndexHTML).append(UVBadge);
-                        console.log(response);
                     })
-                    .catch(function(error) {
-                        alert("uv error")
+                    .catch(function (error) {
+                        $(UVIndexHTML).text("UV Index data unavailable at this time.")
                     });
 
 
+            })
+            .catch(function (error) {
+                $(".weatherCard").css("display", "block");
+                $(".weatherCard").text("Weather data unavailable at this time.");
+            });
+
+        // 5-Day Forecast 
+        var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "&units=imperial" + APIKey;
+
+        $.ajax({
+            url: fiveDayQueryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log("Five day")
                 console.log(response);
             })
-            .catch(function(error) {
-                alert("error");
+            .catch(function (error) {
+                alert("Forecast error")
             });
     });
 });
