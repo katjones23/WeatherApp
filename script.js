dfd38260c89d41d8e1eb78a74036fbda49088a7b
Katherine Jones <kejone1414@gmail.com>
2020-04-09 22:15:03 -0400

    API connection working

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called 
 // "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
 
-// GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
@@ -14,3 +13,30 @@
 // THEN I am again presented with current and future conditions for that city
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
+
+$(document).ready(function () {
+
+    $("button").on("click", function () {
+
+        var inputCity = $("#searchCity").val();
+        console.log(inputCity);
+        var APIKey = "&appid=fc3e557a89901d475f37aaf29507552a";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + APIKey;
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                $(".cityWeather").html("<h3>" + response.name + "</h3>")
+                // var dateUTC = response.dt
+                // var localDate = moment.utc(dateUTC).local().format();
+                console.log(localDate);
+                console.log(response);
+            })
+            .catch(function (error) {
+                alert("error");
+            });
+    });
+});
